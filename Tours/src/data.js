4f3376699a4242ec1703470e2ef1cde6e08a8b81
Thineshraj <tsoosaiselvarajah@myseneca.ca>
2021-10-18 23:47:39 -0400

    tours

---
+++ Tours/src/data.js
@@ -0,0 +1,37 @@
+export const data = [
+  {
+    id: 1,
+    img: 'https://dl.airtable.com/.attachments/a0cd0702c443f31526267f38ea5314a1/2447eb7a/paris.jpg',
+    title: 'Best Of Paris In 7 Days Tour',
+    desc: 'Paris is synonymous with the finest things that culture can offer — in art, fashion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves guide will immerse you in the very best of ...',
+    price: '1,995',
+  },
+  {
+    id: 2,
+    img: 'https://dl.airtable.com/.attachments/6c24084000a3777064c5200a8c2ae931/04081a3e/ireland.jpeg',
+    title: 'Best Of Ireland In 14 Days Tour',
+    desc: "Rick Steves' Best of Ireland tour kicks off with the best of Dublin, followed by Ireland's must-see historical sites, charming towns, music-filled pubs, and seaside getaways — including Kinsale, the D...",
+    price: '3,895',
+  },
+  {
+    id: 3,
+    img: 'https://dl.airtable.com/.attachments/27f6cbfe631e303f98b97e9dafacf25b/6bbe2a07/vienna.jpeg',
+    title: 'Best Of Salzburg & Vienna In 8 Days Tour',
+    desc: "Let's go where classical music, towering castles, and the-hills-are-alive scenery welcome you to the gemütlichkeit of Bavaria and opulence of Austria's Golden Age. Your Rick Steves guide will bring th...",
+    price: '2,695',
+  },
+  {
+    id: 4,
+    img: 'https://dl.airtable.com/.attachments/3efa7aa402d49c12b8769c581a96af42/d5b641e3/italy.jpeg',
+    title: 'Best Of Rome In 7 Days Tour',
+    desc: "Our Rome tour serves up Europe's most intoxicating brew of dazzling art, earth-shaking history, and city life with style. On this Rome vacation, your tour guide will resurrect the grandeur of ancient ...",
+    price: '2,095',
+  },
+  {
+    id: 5,
+    img: 'https://dl.airtable.com/.attachments/3feee7a93af0f4f809312132090c9a80/58e3e8ec/poland.jpeg',
+    title: 'Best Of Poland In 10 Days Tour',
+    desc: "Starting in the colorful port city of Gdańsk, you'll escape the crowds and embrace the understated elegance of ready-for-prime-time Poland for 10 days. With an expert Rick Steves guide at your side, y..",
+    price: '2,595',
+  },
+];
