1a01b1575b440e7b7d9fbd64ba530c585cb2cae0
Thineshraj <tsoosaiselvarajah@myseneca.ca>
2021-10-18 23:53:22 -0400

    tours

---
+++ Tours/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Tours from './Tours';
+
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Tours />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
