1eaa43aa3a8a25fd5932453cbfe6209e2db6f4f4
Thineshraj <tsoosaiselvarajah@myseneca.ca>
2021-11-03 10:08:27 -0400

    color generator

---
+++ Color Generator/src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import SingleColor from './SingleColor';
+
+import Values from 'values.js';
+
+function App() {
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values('#d91e98').all(1));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      const colors = new Values(color).all(1);
+      console.log(colors);
+      setList(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <section className='container'>
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder='#d91e98'
+            className={`${error ? 'error' : null}`}
+          />
+          <button type='submit' className='btn'>
+            Submit
+          </button>
+        </form>
+      </section>
+      <section className='colors'>
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
+}
+
+export default App;
